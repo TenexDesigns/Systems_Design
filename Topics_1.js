2c6837468b41f351c0f95337ba95142d8007541a
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-02 08:54:44 +0300

    Create Topics_1.js

---
+++ Topics_1.js
@@ -0,0 +1,13 @@
+Kafka
+Load Balancing
+Web sockedts - for real time data- This is beeter than the one below
+HTTP polling - for real time data
+Horizontal Database scalling e.g by shard
+Redis
+Spark
+Math for scalling
+Redundecy - in event of system failures to ensure system continues to work
+Data Aalitics
+Algorithms for ETA services
+Algorthms for Shardind divers locations
+
